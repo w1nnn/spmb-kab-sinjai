876ba8da069cc82f91131101829a1c9771a10020
w1nnn <erwin@handayani.ac.id>
2025-04-03 20:09:48 +0800

    Change Spiner Animation After Load Pages

---
+++ application/logs/log-2025-04-03.php
@@ -0,0 +1,34 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+DEBUG - 2025-04-03 20:06:30 --> UTF-8 Support Enabled
+DEBUG - 2025-04-03 20:06:30 --> No URI present. Default controller set.
+DEBUG - 2025-04-03 20:06:30 --> Global POST, GET and COOKIE data sanitized
+DEBUG - 2025-04-03 20:06:31 --> File loaded: C:\xampp\htdocs\spmb-kab-sinjai\application\modules/home/models/Home_model.php
+DEBUG - 2025-04-03 20:06:31 --> File loaded: C:\xampp\htdocs\spmb-kab-sinjai\application\modules/jalur/models/Jalur_model.php
+DEBUG - 2025-04-03 20:06:31 --> File loaded: C:\xampp\htdocs\spmb-kab-sinjai\application\modules/profil/models/Manage_model.php
+DEBUG - 2025-04-03 20:06:31 --> File loaded: C:\xampp\htdocs\spmb-kab-sinjai\application\modules/home/views/landing.php
+DEBUG - 2025-04-03 20:06:31 --> Total execution time: 0.4417
+DEBUG - 2025-04-03 20:07:19 --> UTF-8 Support Enabled
+DEBUG - 2025-04-03 20:07:19 --> No URI present. Default controller set.
+DEBUG - 2025-04-03 20:07:19 --> Global POST, GET and COOKIE data sanitized
+DEBUG - 2025-04-03 20:07:19 --> File loaded: C:\xampp\htdocs\spmb-kab-sinjai\application\modules/home/models/Home_model.php
+DEBUG - 2025-04-03 20:07:19 --> File loaded: C:\xampp\htdocs\spmb-kab-sinjai\application\modules/jalur/models/Jalur_model.php
+DEBUG - 2025-04-03 20:07:19 --> File loaded: C:\xampp\htdocs\spmb-kab-sinjai\application\modules/profil/models/Manage_model.php
+DEBUG - 2025-04-03 20:07:20 --> File loaded: C:\xampp\htdocs\spmb-kab-sinjai\application\modules/home/views/landing.php
+DEBUG - 2025-04-03 20:07:20 --> Total execution time: 0.7257
+DEBUG - 2025-04-03 20:07:23 --> UTF-8 Support Enabled
+DEBUG - 2025-04-03 20:07:23 --> No URI present. Default controller set.
+DEBUG - 2025-04-03 20:07:23 --> Global POST, GET and COOKIE data sanitized
+DEBUG - 2025-04-03 20:07:23 --> File loaded: C:\xampp\htdocs\spmb-kab-sinjai\application\modules/home/models/Home_model.php
+DEBUG - 2025-04-03 20:07:23 --> File loaded: C:\xampp\htdocs\spmb-kab-sinjai\application\modules/jalur/models/Jalur_model.php
+DEBUG - 2025-04-03 20:07:23 --> File loaded: C:\xampp\htdocs\spmb-kab-sinjai\application\modules/profil/models/Manage_model.php
+DEBUG - 2025-04-03 20:07:23 --> File loaded: C:\xampp\htdocs\spmb-kab-sinjai\application\modules/home/views/landing.php
+DEBUG - 2025-04-03 20:07:23 --> Total execution time: 0.3276
+DEBUG - 2025-04-03 20:07:26 --> UTF-8 Support Enabled
+DEBUG - 2025-04-03 20:07:26 --> No URI present. Default controller set.
+DEBUG - 2025-04-03 20:07:26 --> Global POST, GET and COOKIE data sanitized
+DEBUG - 2025-04-03 20:07:27 --> File loaded: C:\xampp\htdocs\spmb-kab-sinjai\application\modules/home/models/Home_model.php
+DEBUG - 2025-04-03 20:07:27 --> File loaded: C:\xampp\htdocs\spmb-kab-sinjai\application\modules/jalur/models/Jalur_model.php
+DEBUG - 2025-04-03 20:07:27 --> File loaded: C:\xampp\htdocs\spmb-kab-sinjai\application\modules/profil/models/Manage_model.php
+DEBUG - 2025-04-03 20:07:27 --> File loaded: C:\xampp\htdocs\spmb-kab-sinjai\application\modules/home/views/landing.php
+DEBUG - 2025-04-03 20:07:27 --> Total execution time: 0.3457
--- application/modules/home/views/landing.php
+++ application/modules/home/views/landing.php
@@ -154,7 +154,7 @@
 
 	<!-- Spinner Start -->
 	<div id="spinner" class="show w-100 vh-100 bg-white position-fixed translate-middle top-50 start-50  d-flex align-items-center justify-content-center">
-		<div class="spinner-grow text-primary" role="status"></div>
+		<div class="spinner-grow text-info" role="status"></div>
 	</div>
 	<!-- Spinner End -->
 
